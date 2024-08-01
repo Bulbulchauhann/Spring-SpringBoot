a9cbf678b4240b04485903a5c90c712945438d5c
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-08-01 23:26:47 +0530

    Dependency Injection with Java File Configuration
    Dependency Injection with Java File Configuration using getters and setters.

---
+++ SpringSP7/src/in/sp/resources/SpringConfigFile.java
@@ -0,0 +1,35 @@
+package in.sp.resources;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import in.sp.beans.Address;
+import in.sp.beans.Student;
+
+@Configuration
+public class SpringConfigFile {
+	
+	@Bean
+	public Address createAddObj() {
+		Address add1 = new Address();
+
+		add1.setHouseno(100);
+		add1.setCity("Jaipur");
+		add1.setPincode(302039);
+
+		return add1;
+	}
+	
+	@Bean
+	public Student createStdObj() {
+
+		Student std1 = new Student();
+
+		std1.setRollno(101);
+		std1.setName("Tina");
+		std1.setAddress(createAddObj());
+		
+		return std1;
+	}
+
+}
