5e447737787797cef58a372de990d3a55d822a26
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-08-12 14:55:21 +0530

    Spring Web MVC Program
    WEB-MVC Program using Controller, and Request Mapping.

---
+++ SpringWebMVC1/src/main/java/in/sp/main/MyController.java
@@ -0,0 +1,29 @@
+package in.sp.main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+//	@RequestMapping(value = "/helloPage", method = RequestMethod.GET)
+	@GetMapping("/helloPage")
+	public ModelAndView openHelloPage() {
+		System.out.println("openHelloPage() method executed");
+
+		ModelAndView mav = new ModelAndView("hello");
+//		mav.setViewName("hello");
+
+		return mav;
+	}
+	
+	
+	@GetMapping("/aboutUs")
+	public String openAboutUsPage() {
+		return "aboutUs";
+	}
+
+}
---
+++ SpringWebMVC1/src/main/webapp/WEB-INF/views/aboutUs.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>About us Page</h2>
+<h4>Contact : +91 80055-27067</h4>
+</body>
+</html>
